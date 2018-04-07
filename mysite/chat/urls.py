7936095536a43efc891455946733d0efd6cbc88a
kevinqq <ezhqing@gmail.com>
2018-04-07 11:45:14 +0800

    docker-compose

---
+++ mysite/chat/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.conf.urls import include, url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    path('<str:room_name>/', views.room, name='room'),
+]
